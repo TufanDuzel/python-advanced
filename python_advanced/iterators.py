fd048ade9d3797aa3857ea6098cb5183dc0c8a06
Tufan Düzel <tufansahind@gmail.com>
2025-02-02 12:45:08 +0100

    python_advanced

---
+++ python_advanced/iterators.py
@@ -0,0 +1,9 @@
+cities = ["Madrid", "Milan", "Atina", "Istanbul"]
+
+iteratorum = iter(cities)
+
+print(next(iteratorum))
+print(next(iteratorum))
+print(next(iteratorum))
+print(next(iteratorum))
+#print(next(iteratorum))
